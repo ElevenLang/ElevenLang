c4bf15f765607d00c5168f87cb45027d0516414b
yuqingfang <yqfhappy@blockheaders.com>
2018-08-21 10:19:32 +0800

    webpack

---
+++ wepack4-demo/config/webpack.config.js
@@ -0,0 +1,45 @@
+// webpack v4
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');// 新加
+
+module.exports = {
+    entry: ['./src/index.js'],
+    // entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['env'] // env转换es6 stage-0转es7 react转react
+                    }
+                }
+            }, 
+            {
+                test: /\.css$/, // 转换文件的匹配正则
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: 'style-loader',
+                    //如果需要，可以在 sass-loader 之前将 resolve-url-loader 链接进来
+                    use: ['css-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin({ filename: 'style.css' }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/tpl/index.html',
+            filename: 'index.html',
+            title: 'test webpack'
+        })// 新加
+
+    ]
+};
---
+++ wepack4-demo/src/js/app.js
@@ -0,0 +1,4 @@
+console.log('app');
+const a = 100;
+console.log(a);
+import "../style.css"
