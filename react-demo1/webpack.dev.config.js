cc0fed072f309abb0e35c72b46337a3a28819582
yuqingfang <yqfhappy@blockheaders.com>
2018-09-05 23:05:17 +0800

    react

--- react-demo1/webpack.dev.config.js
+++ react-demo1/webpack.dev.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 module.exports = {
-    entry: path.join(__dirname, '/src/index'),
+    entry: [
+        'react-hot-loader/patch',
+        path.join(__dirname, '/src/index')
+    ],
     output: {
         path: path.join(__dirname, '/dist/'),
         filename: 'bundle.js'
@@ -11,5 +14,15 @@ module.exports = {
             use: ['babel-loader?cacheDirectory=true'],
             include: path.join(__dirname, 'src')
         }]
+    },
+    resolve: {
+        alias: {
+            pages: path.join(__dirname, 'src/pages'),
+            component: path.join(__dirname, 'src/component'),
+            router: path.join(__dirname, 'src/router')
+        }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './dist')
     }
 }
