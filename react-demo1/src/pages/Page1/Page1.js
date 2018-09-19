c402e5a03cbec1d671f9151fd5d622c77c138fff
yuqingfang <yqfhappy@blockheaders.com>
2018-09-19 23:05:49 +0800

    img

--- react-demo1/src/pages/Page1/Page1.js
+++ react-demo1/src/pages/Page1/Page1.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import './Page1.css';
 import './Page1.scss';
+import image from './images/1.jpg';
+
 export default class Page1 extends Component {
     render() {
         return (
             <div className="page-box"> 
                 this is Page11111~
                 <p>hello</p>
+                <img src={image}/>
             </div>
         )
     }
