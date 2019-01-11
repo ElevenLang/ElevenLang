0bb3b12550a2a8deb72dd8d1a78049fff1388b33
yuqingfang <yqfhappy@blockheaders.com>
2019-01-11 16:12:35 +0800

    add productList page

--- flutter_app_helloworld/lib/main.dart
+++ flutter_app_helloworld/lib/main.dart
@@ -1,47 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+//传递的数据结构,也可以理解为对商品数据的抽象
+class Product{
+  final String title;  //商品标题
+  final String description;  //商品描述
+  Product(this.title,this.description);
+}
 
 
 void main(){
   runApp(MaterialApp(
-    title: '导航演示1',
-    home: new FirstScreen()
+    title:'数据传递案例',
+    home:ProductList(
+      products:List.generate(
+        20, 
+        (i)=>Product('商品 $i','这是一个商品详情，编号为:$i')
+      ),
+    )
   ));
 }
-
-
-
-class FirstScreen extends StatelessWidget{
+class ProductList extends StatelessWidget{
+  final List<Product> products;
+  ProductList({Key key,@required this.products}):super(key:key);
   @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: AppBar(title: Text('导航页面')),
-      body: Center(
-        child: RaisedButton(
-          child: Text('查看商品详情页面'),
-          onPressed: (){
-            Navigator.push(context, new MaterialPageRoute(
-              builder: (context) =>new SecondScreen()
-            ));
-          }
-        ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text('商品列表')),
+      body:ListView.builder(
+        itemCount:products.length,
+        itemBuilder: (context,index){
+          return ListTile(
+            title:Text(products[index].title),
+            onTap:(){
+              _getDetails(context, products, index);
+            }
+          );
+        },
       )
     );
   }
 }
 
-class SecondScreen extends StatelessWidget{
+_getDetails(context, products, index){
+  print(products);
+  Navigator.push(
+    context, 
+    MaterialPageRoute(
+      builder:(context)=>new ProductDetail(product:products[index])
+    )
+  );
+}
+class ProductDetail extends StatelessWidget {
+  final Product product;
+  ProductDetail({Key key ,@required this.product}):super(key:key);
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar:AppBar(title:Text('技术胖商品详情页')),
-      body: Center(
-        child: RaisedButton(
-          child: Text('返回'),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
-      )
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title:Text('${product.title}'),
+      ),
+      body:Center(child: Text('${product.description}'),)
     );
   }
 }
