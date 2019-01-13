f5e0ea093ff7dc07e88cfbafc03affce9df3886c
yuqingfang <yqfhappy@163.com>
2019-01-13 22:22:45 +0800

    sanckbar

--- flutter_app_helloworld/lib/main.dart
+++ flutter_app_helloworld/lib/main.dart
@@ -1,68 +1,38 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(SnackBarDemo());
 
-void main(){
-  runApp(MaterialApp(
-    title:'页面跳转返回数据',
-    home:FirstPage()
-  ));
-}
-class FirstPage extends StatelessWidget {
+class SnackBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(title:Text("找小姐姐要电话")),
-      body:Center(
-        child: RouteButton(),
+    return MaterialApp(
+      title: 'SnackBar Demo',
+      home:Scaffold(
+        appBar: AppBar(title: Text('SnackBar Demo')),
+        body: SnackBarPage(),
       )
     );
   }
 }
-//跳转的Button
-class RouteButton extends StatelessWidget {
+
+class SnackBarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed:(){
-          _navigateToXiaoJieJie(context);
-      },
-      child: Text('去找小姐姐'),
-    );
-  }
-  _navigateToXiaoJieJie(BuildContext context) async{ //async是启用异步方法
-    final result = await Navigator.push(//等待
-      context, 
-      MaterialPageRoute(builder: (context)=> XiaoJieJie())
-      );
-      print(result);
-      Scaffold.of(context).showSnackBar(SnackBar(content:Text('$result')));
-  }
-}
-class XiaoJieJie extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        title:Text('我是小姐姐')
+    return Center(
+      child: RaisedButton(
+        child: Text('show snack bar'),
+        onPressed: (){
+          final snackBar = SnackBar(
+            content: Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'undo',
+              onPressed: (){}
+            ),
+          );
+          // Find the Scaffold in the Widget tree and use it to show a SnackBar
+          Scaffold.of(context).showSnackBar(snackBar);
+        }
       ),
-      body:Center(
-        child:Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('大长腿小姐姐'),
-              onPressed: (){
-                Navigator.pop(context,'大长腿:1511008888');
-              },
-            ) ,
-            RaisedButton(
-              child: Text('小蛮腰小姐姐'),
-              onPressed: (){
-                Navigator.pop(context,'大长腿:1511009999');
-              },
-            ) ,
-          ],
-        ) 
-      ) ,
     );
   }
 }
